c7a3f7324d8d773e12e60bb1d55ae6be0c3f7e35
Liemka2k6 <ngovanliem8a@gmail.com>
2025-09-23 14:00:59 +0700

    add introdution

---
+++ HW1/Introdution.java
@@ -0,0 +1,9 @@
+public class Introdution {
+    public static void main(String[] args) {
+        System.out.println("Ngô Văn Liêm\t24020199\tK69I-IT1\tLiemka2k6\t24020199@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.\nTake one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
